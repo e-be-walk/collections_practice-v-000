a522942ec37405cece28860b1d686b8ca74130f9
Erin Walker <erinb0317@hotmail.com>
2018-01-30 14:21:54 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort { |a, b| b <=> a}
+end
+
+
+def sort_array_char_count(array)
+  array.sort_by { |word| word.length }
+end
+
+def swap_elements(array)
+#no luck here- need to first assign an index to the array, then rearrange the items by index
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker
+end
+
+def find_a(array)
+  array.each do |name|
+    if name.start_with?("a")
+      "#{name}"
+    end
+end
